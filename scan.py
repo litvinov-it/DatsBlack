df285ad8ee1fc3fdb04136ec301d798ee803cf37
Danil <litan.yt.terr@gmail.com>
2023-12-23 20:13:05 +0700

    считаем наши и вражеские корябли по близости

---
+++ scan.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = 'https://datsblack.datsteam.dev/api'
+headers = {
+    "x-api-Key": "dff47333-0c90-4954-be6f-60fa24043181"
+}
+
+response = requests.get(f"{BASE_URL}/scan", headers=headers)
+
+data = response.json()
+
+
+
+print('Наших кораблей: ', len(data['scan']['myShips']))
+print('Вражеских кораблей рядом: ', len(data['scan']['enemyShips']))
