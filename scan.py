ff7d349fca1554aea6bbdc704b41dbe53a218d43
Danil <litan.yt.terr@gmail.com>
2023-12-23 21:58:05 +0700

    добавил предикт координат начал работу с картой

--- scan.py
+++ scan.py
@@ -9,7 +9,5 @@ response = requests.get(f"{BASE_URL}/scan", headers=headers)
 
 data = response.json()
 
-
-
 print('Наших кораблей: ', len(data['scan']['myShips']))
 print('Вражеских кораблей рядом: ', len(data['scan']['enemyShips']))
